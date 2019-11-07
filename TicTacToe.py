a1e094b15051ecc8a988c8965b60fbc221fbb8bd
Kevin Palomeque <kevin97ny@gmail.com>
2019-11-07 15:53:14 -0500

    push

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,5 +1,6 @@
 from random import randint
 
+# create our tiles
 tiles = {
     1: ['-', '-', '-'],
     2: ['-', '-', '-'],
@@ -8,13 +9,15 @@ tiles = {
 
 game_over = False
 
-chars = ['x', 'o']
-pc_char = ''
-user_char: str = ''
-matches = 0
+chars = ['x', 'o']  # array to hold available characters
+pc_char = ''  # the pc ( opponent)
+user_char: str = ''  # the user
 
+# set up our variables ( values are arbitrary )
 row = 1
 column = 0
+
+matches = 0
 
 
 def display_greeting():  # welcome the user
@@ -30,8 +33,8 @@ def assign_characters():  # assign characters (x,o) to the user and pc
 
     while True:
         if user_char == 'x' or user_char == 'o':
-            chars.remove(user_char)
-            pc_char = chars[0]
+            chars.remove(user_char)  # remove the user's character from the characters array
+            pc_char = chars[0]  # assign the remain character to the pc
             print("You are playing as " + user_char.title() + ".")
             break
         else:
@@ -80,7 +83,7 @@ def select_tile():
 
 def check_tile():
     while True:
-        if tiles[row][column - 1] != '-':
+        if tiles[row][column - 1] != '-':  # if the space is occupied by anything other than a dash
             print("\nThat tile is occupied, please choose another tile.")
             select_tile()
         else:
@@ -101,6 +104,7 @@ def pc_move():
     global pc_char
 
     while True:
+        # select random values for the pc
         row = randint(1, 3)
         column = randint(0, 2)
         if tiles[row][column] != '-':  # if the space is occupied
@@ -117,13 +121,12 @@ def pc_move():
         user_move()
 
 
-def game_state_checker(char, row):
+def game_state_checker(char, row):  # function to check for three in a row
     global game_over
     global matches
 
     matches = 0
     check_char = char
-    row = row
 
     if check_char == user_char:
         player = "You"
@@ -168,10 +171,10 @@ def game_state_checker(char, row):
         else:
             pass
         column -= 1
-    win_checker(player)
+    win_checker(player)  # check to see if anyone won, passing the current player and number of matches
 
 
-def win_checker(player):
+def win_checker(player):  # check if the user or the pc is the winner
     global game_over
     global matches
 
